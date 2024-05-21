ba75dc49c371795e5990f98c70ceeb4647545773
darkdomian <darkdomian@gmail.com>
2024-05-21 23:22:50 +0400

    update

--- FreeCodeCamp/LohnAlgorithm.py
+++ FreeCodeCamp/LohnAlgorithm.py
@@ -1,7 +1,7 @@
 def verify_card_number(card_number):
     sum_of_odd_digits = 0
     card_number_reversed = card_number[::-1]
-    odd_digits    
+    # odd_digits
 def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
