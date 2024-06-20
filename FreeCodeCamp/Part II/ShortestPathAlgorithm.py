0fabafeb1b14d9035ff019dcbff41b8f617c443a
darkdomian <darkdomian@gmail.com>
2024-06-20 17:39:29 +0300

    graph tutorial on freecodecamp

--- FreeCodeCamp/Part II/ShortestPathAlgorithm.py
+++ FreeCodeCamp/Part II/ShortestPathAlgorithm.py
@@ -1,5 +1,32 @@
 my_graph = {
-    'species': 'guinea pig',
-    'age': 2
+    'A': [('B', 3), ('D', 1)],
+    'B': [('A', 3), ('C', 4)],
+    'C': [('B', 4), ('D', 7)],
+    'D': [('A', 1), ('C', 7)]
 }
-#green day
+
+def shortest_path(graph, start):
+    unvisited = list(graph)
+    distances = {node: 0 if node == start else float('inf') for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
+    while unvisited:
+        current = min(unvisited, key=distances.get)
+        for node, distance in graph[current]:
+            if distance + distances[current] < distances[node]:
+                distances[node] = distance + distances[current]
+    
+    # actialy I don't need this loop to achive this result
+
+    # unvisited = []
+    # distances = {}
+
+    # for node in graph:
+    #     unvisited.append(node)
+    #     if node == start:
+    #         distances[node] = 0
+    #     else:
+    #         distances[node] = float('inf')
+    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
+    
+shortest_path(my_graph, 'A')
