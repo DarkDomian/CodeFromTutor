cf27734dc687a9f69cd7c52ca6e930815c8e167d
darkdomian <darkdomian@gmail.com>
2024-06-29 20:30:23 +0300

    10 line of code

--- FreeCodeCamp/Part II/TowerOfHanoi.py
+++ FreeCodeCamp/Part II/TowerOfHanoi.py
@@ -9,8 +9,24 @@ rods = {
 def move(n, source, auxiliary, target):
     # display starting configuration
     print(rods)
+
     for i in range(number_of_moves):
-        print(i)
+        remainder = (i + 1) % 3
+        if remainder == 1:
+            print(f'Move {i + 1} allowed between {source} and {target}')
+            forward = False
+            if not rods[target]:
+                forward = True
+            elif rods[source] and rods[source][-1] < rods[target][-1]:
+                forward = True
+            if forward:
+                print(f'Moving disk {rods[source][-1]} from {source} to {target}')
+                # rods[source].pop(rods[source][-1])
+                # rods[target].append(rods[source].pop(rods[source][-1]))
+
+    # useless lines :
+    # for i in range(number_of_moves):
+    #     print(i)
 
 # initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')
