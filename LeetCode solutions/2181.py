6527bc61b8a454198796dd88ae9a2a7cf7afeeec
darkdomian <darkdomian@gmail.com>
2024-07-05 22:09:35 +0300

    0 lines of code

---
+++ LeetCode solutions/2181.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        modify = head.next
+        next_sum = modify
+
+        while next_sum:
+            sum = 0
+            
+            while next_sum.val != 0:
+                sum += next_sum.val
+                next_sum = next_sum.next
+
+            modify.val = sum
+
+            next_sum = next_sum.next
+
+            modify.next = next_sum
+            modify = modify.next
+        return head.next
